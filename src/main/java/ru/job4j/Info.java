1bc227660a47b4c598a743b4388d07346f630077
smirnov-maksim-1984 <maksimus.84@list.ru>
2022-08-29 06:59:38 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Info {
         public static void main(String[] args) {
-            System.out.println("26.08.2022");
-}
+            System.out.println("29.08.2022");
+        }
 
-    }
+}
