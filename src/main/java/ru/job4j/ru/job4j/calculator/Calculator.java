99da4157e430baf3a1fdcffeca3980da2ab51749
smirnov-maksim-1984 <maksimus.84@list.ru>
2022-10-10 07:49:58 +0300

    3. Арифметические операции. [#185 #338132]

--- src/main/java/ru/job4j/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,35 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+    @SuppressWarnings({"checkstyle:EmptyLineSeparator", "checkstyle:WhitespaceAfter"})
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
+        int onePlusTwo = one + two; //System.out.println(onePlusTwo);
+
         int six = 6;
         int four = 4;
         int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        int sixDivTwo = six / two; //System.out.println(sixDivTwo);
+        int fiveMinusTwo = five - two; //System.out.println(fiveMinusTwo);
+        int fourTimeTwo = four * two; //System.out.println(fourTimeTwo);
+
+
+    }
+}
+
+class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
     }
 }
