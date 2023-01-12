14811b8285281a7e250f2fe8579361cfcaa3a786
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-12 16:18:24 +0300

    3. Арифметические операции. [#185 #338132]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -4,7 +4,6 @@ public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
-        int onePlusTwo = one + two;
 
         int six = 6;
         int four = 4;
