d9e007ec249357664ccda15680a6bca96107af06
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-13 11:13:30 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -7,8 +7,8 @@ public class MathFunc {
     }
 
        public static int func2(int x) {
-        int y = 1 / x;
-        return y;
+           int y = 1 / x;
+           return y;
     }
 
     public static void main(String[] args) {
