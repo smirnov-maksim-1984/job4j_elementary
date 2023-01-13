cb6765abf655419bf341c35cbe10640850ebc6aa
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-13 17:43:41 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -11,7 +11,6 @@ public class Converter {
         float rsl = value / 60;
         return rsl;
     }
-
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
