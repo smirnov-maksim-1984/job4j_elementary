8cb39651e7368810a7d1f9d7c38d1037ab42cf8e
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-18 14:00:07 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,6 +3,7 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
+
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                 + multiply(first, second);
@@ -13,7 +14,7 @@ public class MathCalculator {
                 + div(first, second);
     }
 
-    public static double SumAllOperations(double first, double second) {
+    public static double sumAllOperations(double first, double second) {
         return sum(first, second)
                 + multiply(first, second)
                 + subtraction(first, second)
@@ -23,6 +24,6 @@ public class MathCalculator {
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета метода 2 равен: " + sumSubtractionAndDiv(10, 20));
-        System.out.println("Результат расчета метода 3 равен: " + SumAllOperations(10, 20));
+        System.out.println("Результат расчета метода 3 равен: " + sumAllOperations(10, 20));
     }
 }
