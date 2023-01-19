a3b2e523ed077940cb4b0910b4c6a24c32c2e7ca
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-19 23:12:25 +0300

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,12 +2,17 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
+        result = Point.distance(1, 3, 3, 5);
+        System.out.println("result (1, 3) to (3, 5) " + result);
+        result = Point.distance(1, 1, 2, 3);
+        System.out.println("result (1, 1) to (2, 3) " + result);
+        result = Point.distance(3, 3, 7, 5);
+        System.out.println("result (3, 3) to (7, 5) " + result);
         }
     }
