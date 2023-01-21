02239bdd82e88f4e1edd806a43751bdb8bfe7778
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-21 09:44:31 +0300

    4.1. Операторы ветвлений [#3611 #383335]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -4,9 +4,9 @@ public class AlertDivByZero {
     public static void possibleDiv(int number) {
         if (number == 0) {
             System.out.println("Could not div by 0.");
-            if (number < 0) {
-                System.out.println("This is negative number");
-            }
+        }
+        if (number < 0) {
+            System.out.println("This is negative number");
         }
     }
 
