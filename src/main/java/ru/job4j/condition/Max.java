bd3159c5b45ee1c68cb89f8df4a7a372c6e1543f
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-23 17:35:46 +0300

    4.2. Тернарное сравнение [#189 #384041]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -13,11 +13,11 @@ public class Max {
         return result;
     }
     public static int max(int left, int right) {
-        boolean b = right == left;
-        int result = b ? right : left;
+        boolean с = right == left;
+        int result = с ? right : left;
         return result;
 
-        public static void main(String [] args) {
+        public static void main(String[] args) {
         int maximum = Max.max(3, 2);
         System.out.println(maximum);
     }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -12,4 +12,22 @@ public class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax2To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
 }
