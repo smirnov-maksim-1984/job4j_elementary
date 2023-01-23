c15510057ac153af5af8deb352454f6c2d5160b3
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-23 17:29:17 +0300

    4.2. Тернарное сравнение [#189 #384041]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -12,6 +12,11 @@ public class Max {
         int result = b ? right : left;
         return result;
     }
+    public static int max(int left, int right) {
+        boolean b = right == left;
+        int result = b ? right : left;
+        return result;
+
         public static void main(String [] args) {
         int maximum = Max.max(3, 2);
         System.out.println(maximum);
