75687d9d613f7d389fd64f4bc97fe47250af3978
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-01-26 12:39:55 +0300

    4.2. Тернарное сравнение [#189 #384041]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,31 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max1(int left, int right) {
+    public static int max(int left, int right) {
         boolean a = left > right;
         int result = a ? left : right;
         return result;
     }
 
-    public static int max2(int left, int right) {
-        boolean b = right > left;
-        int result = b ? right : left;
-        return result;
-    }
-
-    public static int equals(int left, int right) {
-        boolean c = right == left;
-        int result = c ? right : left;
-        return result;
-    }
-
     public static void main(String[] args) {
-        int firstMaxNumber = Max.max1(1, 2);
-        int secondMaxNumber = Max.max2(3, 5);
-        int equalNumbers = Max.equals(3, 3);
-        System.out.println(firstMaxNumber);
-        System.out.println(secondMaxNumber);
-        System.out.println(equalNumbers);
+        int maximumNumber = Max.max(1, 2);
+        System.out.println(maximumNumber);
     }
 }
 
