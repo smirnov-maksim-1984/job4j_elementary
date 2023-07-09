21dbe41fe5f0a2a78b15cb893cdb46baf28b78e0
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-07-09 17:36:05 +0300

    3.0. Импорт проекта в IDEA [#160822 #290803]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,11 +2,9 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Привет, Мир!");
-=======
 
-        System.out.println("Привет мир");
->>>>>>> 4066726 (3.2. Загрузка файлов на github [#243562#333550])
+        System.out.println("Привет, Мир!");
+
+
     }
 }
