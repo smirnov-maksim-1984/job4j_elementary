8ec1365ae5724270f5fa2391626bc9258138408b
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-09-08 12:05:38 +0300

    4.5. Отладка программы в IDEA [#159464]

---
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class ThreeMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if (first < second && first < third) {
+            result = third;
+        }
+        if (second > first && second > third) {
+            result = second;
+        }
+        return result;
+    }
+}
+
