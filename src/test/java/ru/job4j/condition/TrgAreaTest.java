19731c968de9c3743606b0de005e30b06b17edde
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-09-08 12:44:56 +0300

    9. Площадь треугольника. [175474 #381715]

---
+++ src/test/java/ru/job4j/condition/TrgAreaTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrgAreaTest {
+    public static double area(double a, double b, double c) {
+        double rsl = -1;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + rsl);
+    }
+}
