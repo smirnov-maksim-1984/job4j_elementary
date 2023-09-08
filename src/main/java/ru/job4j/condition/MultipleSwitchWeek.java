3d6dea40f5e3a2ade9a732bf2fd2dc91f57e2738
smirnov-maksim-1984 <maksimus.84@list.ru>
2023-09-08 18:12:08 +0300

    4.6.2. Switch - matcher [#334071]

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -5,7 +5,7 @@ public class MultipleSwitchWeek {
         return switch (name) {
             case "Понедельник", "Monday" -> 1;
             case "Вторник", "Tuesday" -> 2;
-            case "Среда", "Wensday" -> 3;
+            case "Среда", "Wednesday" -> 3;
             case "Четверг", "Thursday" -> 4;
             case "Пятница", "Friday" -> 5;
             case "Суббота", "Saturday" -> 6;
